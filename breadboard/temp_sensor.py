5d43c3eba69a15a223dcd3a3b9c8319cf831dfe8
ryan-rashidian <ryanrashidian@gmail.com>
2025-08-11 02:16:35 -0700

    Add temp_sensor.py for onboard temperature reading test

---
+++ breadboard/temp_sensor.py
@@ -0,0 +1,25 @@
+from machine import ADC # type: ignore
+from time import sleep
+
+sensor_temp = ADC(4)
+
+def read_temp():
+    reading = sensor_temp.read_u16()
+    voltage = (reading / 65535) * 3.3
+    # Formula from RP2350 datasheet (12.4.6 Temperature Sensor)
+    temp_c = 27 - (voltage - 0.706)/0.001721
+    return temp_c
+
+def loop():
+    while True:
+        temp_c = read_temp()
+        print(f'Internal Temperature: {temp_c:.2f} °C')
+        sleep(1)
+
+if __name__ == '__main__':
+    try:
+        print('Start Program.')
+        loop()
+    except KeyboardInterrupt:
+        print('End Program.')
+
