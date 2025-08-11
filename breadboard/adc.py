3a87b4605795d3cccff0c4fcbad334139b9dcb5d
ryan-rashidian <ryanrashidian@gmail.com>
2025-08-10 22:24:14 -0700

    Added adc.py to for testing pico ADC with potentiometer

---
+++ breadboard/adc.py
@@ -0,0 +1,19 @@
+from machine import ADC, Pin # type: ignore
+from time import sleep
+
+adc = ADC(Pin(26))
+
+def loop():
+    while True:
+        raw = adc.read_u16()
+        volts = (raw / 65535) * 3.3
+        print(f'Raw: {raw} | Voltage: {volts}')
+        sleep(0.1)
+
+if __name__ == '__main__':
+    try:
+        print('Start Program.')
+        loop()
+    except KeyboardInterrupt:
+        print('End Program.')
+
